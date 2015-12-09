08550d0dafb7f30efbb91045218e22e9c6bf563f
ayeshabhatti <ayesha.sami@gmail.com>
2015-12-09 08:46:52 +0100

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,5 +1,5 @@
 
-
+  #I have done some calculations off code and by skipping 666637 rows, I have managed to load my desired data i.e. 2880 rows 
   dt<-read.table("expdataproject1/household_power_consumption.txt",sep=";", nrows = 2880,skip = 66637,stringsAsFactors = FALSE)
   cnames<-read.table("expdataproject1/household_power_consumption.txt",sep=";", nrows = 1,stringsAsFactors = FALSE)
   colnames(dt)<-cnames
