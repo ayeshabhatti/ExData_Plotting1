248d56fced708beb3ff1d16a0528d9d1d4b998e1
ayeshabhatti <ayesha.sami@gmail.com>
2015-12-09 08:52:34 +0100

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -4,16 +4,19 @@
   colnames(dt)<-cnames
   
   dt$Date <- as.Date(dt$Date,"%d/%m/%Y")
-  
+  #The following step is done to get the days correctly scaled on x-axis
   dt1<-within(dt, { timestamp=as.POSIXct(paste(Date, Time)) })
   png("ExData_Plotting1/plot3.png", width=480, height=480)
+  #plot the graph
   plot(dt1$timestamp,dt1$Sub_metering_1,type = "n", xlab="", ylab="Energy of submetering")
+  #plot the lines
   lines(dt1$timestamp,dt1$Sub_metering_1,type = "l")
   lines(dt1$timestamp,dt1$Sub_metering_2,type = "l",col= "red")
   lines(dt1$timestamp,dt1$Sub_metering_3,type = "l",col= "blue")
-  
+  #write legend
   legend("topright",col=c("black","red", "blue"), lwd=2,legend=c("sub_metering_1","sub_metering_2","sub_metering_3"))
- 
+  #close the device 
   dev.off()
+  #delete objects from workspace
   rm(dt,dt1,cnames)
 
